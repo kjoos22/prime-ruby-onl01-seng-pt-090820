28160092bc1cc245421aded195082f23ebe31ae9
Kristian Joos <kjoos22@gmail.com>
2020-09-16 21:17:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@
 require 'pry'
 
 def prime?(number)
-  range = (2..number-1).to_a
+  range = (2..number).to_a
   
   if number < 1
     return false
   end
   range.each do |num|
-    if number % num == 0
+    if number % num == 0 && num != number
       return false
     end
   end
